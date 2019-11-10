d2895966c964ae0c7618d944433bcd95d4fecad0
Emilien Garreau <emilien.garreau@gmail.com>
2019-11-10 14:21:07 +0100

    Remove tf.TestCase from test losses

--- src/od/core/losses.py
+++ src/od/core/losses.py
@@ -2,9 +2,6 @@ import tensorflow as tf
 
 import tensorflow.keras.losses as losses
 
-from tensorflow.python.keras.utils.generic_utils import deserialize_keras_object
-from tensorflow.python.keras.utils.generic_utils import serialize_keras_object
-
 
 class SmoothL1Localization(losses.Huber):
     """Smooth L1 localization loss function aka Huber Loss..
@@ -60,35 +57,37 @@ class CategoricalCrossentropy(losses.CategoricalCrossentropy):
         return super().call(y_true, y_pred)
 
 
-class FocalBinaryCrossentropy(losses.Loss):
-    """Focal binary cross entropy loss.
+# class FocalCategoricalCrossentropy(losses.Loss):
+#     """Focal categorical cross entropy loss.
 
-    Focal loss down-weights well classified examples and focusses on the hard
-    examples. See https://arxiv.org/pdf/1708.02002.pdf for the loss definition.
-    """
+#     Focal loss down-weights well classified examples and focusses on the hard
+#     examples. See https://arxiv.org/pdf/1708.02002.pdf for the loss definition.
+#     """
 
-    def __init__(self, gamma=2.0, alpha=0.25, name='focal_loss'):
-        """Constructor.
+#     def __init__(self, gamma=2.0, alpha=0.25, name='focal_loss'):
+#         """Constructor.
 
-        Arguments:
+#         Arguments:
 
-        - *gamma*: exponent of the modulating factor (1 - p_t) ^ gamma.
-        - *alpha*: optional alpha weighting factor to balance positives vs negatives.
-        """
-        super().__init__(reduction=tf.keras.losses.Reduction.NONE, name=name)
-        self._alpha = alpha
-        self._gamma = gamma
+#         - *gamma*: exponent of the modulating factor (1 - p_t) ^ gamma.
+#         - *alpha*: optional alpha weighting factor to balance positives vs negatives.
+#         """
+#         super().__init__(reduction=tf.keras.losses.Reduction.NONE, name=name)
+#         self._alpha = alpha
+#         self._gamma = gamma
 
-    def call(self, y_true, y_pred):
-        per_entry_cross_ent = (losses.binary_cross_entropy(y_true, y_pred, from_logits=True))
-        prediction_probabilities = tf.sigmoid(y_pred)
-        p_t = ((y_true * prediction_probabilities) + ((1 - y_true) *
-                                                      (1 - prediction_probabilities)))
-        modulating_factor = 1.0
-        if self._gamma:
-            modulating_factor = tf.pow(1.0 - p_t, self._gamma)
-        alpha_weight_factor = 1.0
-        if self._alpha is not None:
-            alpha_weight_factor = (y_true * self._alpha + (1 - y_true) * (1 - self._alpha))
-        focal_cross_entropy_loss = (modulating_factor * alpha_weight_factor * per_entry_cross_ent)
-        return focal_cross_entropy_loss
+#     def call(self, y_true, y_pred):
+
+#         per_entry_cross_ent = (losses.cateforical_cross_entropy(y_true, y_pred, from_logits=True))
+#         prediction_probabilities = tfkl.actications.softmax(y_pred)
+
+#         p_t = ((y_true * prediction_probabilities) + ((1 - y_true) *
+#                                                       (1 - prediction_probabilities)))
+#         modulating_factor = 1.0
+#         if self._gamma:
+#             modulating_factor = tf.pow(1.0 - p_t, self._gamma)
+#         alpha_weight_factor = 1.0
+#         if self._alpha is not None:
+#             alpha_weight_factor = (y_true * self._alpha + (1 - y_true) * (1 - self._alpha))
+#         focal_cross_entropy_loss = (modulating_factor * alpha_weight_factor * per_entry_cross_ent)
+#         return focal_cross_entropy_loss
