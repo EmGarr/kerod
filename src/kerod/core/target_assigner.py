Cast proper tensors to fix the mixed precision

src/kerod/core/target_assigner.py
@@ -111,8 +111,14 @@ class TargetAssigner:
         reg_weights = self._create_regression_weights(groundtruth_weights, matched_labels)
         cls_weights = self._create_classification_weights(groundtruth_weights, matched_labels)
 
-        y_true = {LossField.CLASSIFICATION: cls_targets, LossField.LOCALIZATION: reg_targets}
-        weights = {LossField.CLASSIFICATION: cls_weights, LossField.LOCALIZATION: reg_weights}
+        y_true = {
+            LossField.CLASSIFICATION: tf.cast(cls_targets, self.dtype),
+            LossField.LOCALIZATION: tf.cast(reg_targets, self.dtype)
+        }
+        weights = {
+            LossField.CLASSIFICATION: tf.cast(cls_weights, self.dtype),
+            LossField.LOCALIZATION: tf.cast(reg_weights, self.dtype)
+        }
 
         return y_true, weights
 
@@ -238,7 +244,7 @@ class TargetAssigner:
         A tensor of shape [batch_size, num_anchors] representing classification weights.
         """
         indicator = matched_labels < 0
-        weights = tf.where(indicator, 0., groundtruth_weights)
+        weights = tf.where(indicator, tf.constant(0., dtype=self.dtype), groundtruth_weights)
         indicator = matched_labels == 0
-        weights = tf.where(indicator, 1., weights)
+        weights = tf.where(indicator, tf.constant(1, dtype=self.dtype), weights)
         return weights

src/kerod/model/detection/fast_rcnn.py
@@ -240,8 +240,9 @@ class FastRCNN(AbstractDetectionHead):
         - *classification_loss*: A scalar
         - *localization_loss*: A scalar
         """
+        y_true_classification = tf.cast(y_true[LossField.CLASSIFICATION], tf.int32)
         accuracy, fg_accuracy, false_negative = compute_fast_rcnn_metrics(
-            y_true[LossField.CLASSIFICATION], classification_pred)
+            y_true_classification, classification_pred)
         self.add_metric(accuracy, name='accuracy', aggregation='mean')
         self.add_metric(fg_accuracy, name='fg_accuracy', aggregation='mean')
         self.add_metric(false_negative, name='false_negative', aggregation='mean')
@@ -251,9 +252,7 @@ class FastRCNN(AbstractDetectionHead):
         batch_size = tf.shape(classification_pred)[0]
         # We create a boolean mask to extract the desired localization prediction to compute
         # the loss
-        one_hot_targets = tf.one_hot(y_true[LossField.CLASSIFICATION],
-                                     self._num_classes,
-                                     dtype=tf.int8)
+        one_hot_targets = tf.one_hot(y_true_classification, self._num_classes, dtype=tf.int8)
         one_hot_targets = tf.reshape(one_hot_targets, [-1])
 
         # We need to insert a fake background classes at the position 0

src/kerod/model/faster_rcnn.py
@@ -114,7 +114,8 @@ class FasterRcnnFPN(tf.keras.Model):
         if training and not self._serving:
             ground_truths = inputs['ground_truths']
             # Include the ground_truths as RoIs for the training
-            rois = tf.concat([rois, ground_truths[BoxField.BOXES]], axis=1)
+            rois = tf.concat([tf.cast(rois, self._compute_dtype), ground_truths[BoxField.BOXES]],
+                             axis=1)
             # Sample the boxes needed for inference
             y_true, weights, rois = self.fast_rcnn.sample_boxes(rois, ground_truths)
 

tests/model/detection/test_fast_rcnn.py
@@ -82,7 +82,7 @@ def test_fast_rcnn_sample_boxes(mock_shuffle):
 
     np.testing.assert_array_equal(expected_y_true_classification, y_true[LossField.CLASSIFICATION])
     np.testing.assert_array_almost_equal(expected_y_true_localization,
-                                         y_true[LossField.LOCALIZATION])
+                                         y_true[LossField.LOCALIZATION], decimal=4)
     np.testing.assert_array_equal(expected_weights_classification,
                                   weights[LossField.CLASSIFICATION])
     np.testing.assert_array_equal(expected_weights_localization, weights[LossField.LOCALIZATION])