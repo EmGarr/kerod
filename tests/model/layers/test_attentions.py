cc78ea772c77cd58b79956e01c4a04f06a632c82
Emilien Garreau <emilien.garreau@gmail.com>
2021-01-21 16:53:44 +0100

    [layers/attention] Move Multihead attention to attention

---
+++ tests/model/layers/test_attentions.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+from kerod.model.layers.attentions import MultiHeadAttention
+
+
+def test_multihead_attention_no_mask():
+    d_model = 16
+    value, key, query = [
+        tf.random.uniform(shape) for shape in ((2, 4, 10), (2, 4, 10), (2, 10, 10))
+    ]
+    mha = MultiHeadAttention(d_model, 4)
+    assert mha((value, key, query, None)).shape == (2, 10, 16)
+
+
+def test_multihead_attention_with_mask():
+    d_model = 16
+    value, key, query = [
+        tf.random.uniform(shape) for shape in ((2, 4, 10), (2, 4, 10), (2, 10, 10))
+    ]
+
+    key_padding_mask = tf.ones((2, 4), tf.bool)
+    mha = MultiHeadAttention(d_model, 4)
+    assert mha((value, key, query, key_padding_mask)).shape == (2, 10, 16)
