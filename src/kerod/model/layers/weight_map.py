7baa8210535762f6ec4b84b812c9bdf6e86a29d1
Emilien Garreau <emilien.garreau@gmail.com>
2021-01-31 15:12:49 +0100

    fix weight_map

--- src/kerod/model/layers/weight_map.py
+++ src/kerod/model/layers/weight_map.py
@@ -11,73 +11,68 @@ class DynamicalWeightMaps(tf.keras.layers.Layer):
 
     Example:
 
-    This is a spatial weight map on a 600 x 1200 image with a reference point
-    of y=0.62, x=0.73, height=0.33 and width=0.31.
+    This is an example of spatial weight maps from a 20 x 20 tensor and where we generated
+    25 random reference points.
 
     ![Spatial weight map](https://raw.githubusercontent.com/EmGarr/cv/master/ressources/spatial_weight_map.png)
+
+    This spatial_weight_map is then flatten to obtain a [25, 400] matrix representing the
+    the modulated co-attention.
+
+    ![Spatial weight map flatten](https://raw.githubusercontent.com/EmGarr/cv/master/ressources/flatten_weight_map.png)
 
     Args:
         beta: Modulate the bandwidth of the Gaussian-like distribution.
 
     Call arguments:
-        height: The targeted height
-        width: The targeted width
+        height: The targeted height of the feature map
+        width: The targeted width of the feature map
         ref_points: A tensor of shape [batch_size, N, heads, (y, x, h, w)] in [0, 1]
 
     Returns:
         weight_map: A 4D tensor of float 32 and shape
-            [batch_size, N, heads, height, width] representing
+            [batch_size, heads, N, height * width] representing
             a weight map per reference points.
     """
 
     def __init__(self, beta=1.):
         self._beta = tf.convert_to_tensor(beta)
 
-    def call(self, height, width, ref_points):
+    def call(self, height: int, width: int, ref_points: tf.Tensor):
         """
 
         Args:
-            height: The targeted height
-            width: The targeted width
+            height: The targeted height of the feature map
+            width: The targeted width of the feature map
             ref_points: A tensor of shape [batch_size, N, heads, (y, x, h, w)] in [0, 1]
 
         Returns:
             weight_map: A 4D tensor of float 32 and shape
-                [batch_size, N, heads, height, width] representing
+                [batch_size, N, heads, height * width] representing
                 a weight map per reference points.
         """
+
         x = tf.cast(tf.linspace(0, 1, width), self.dtype)
         y = tf.cast(tf.linspace(0, 1, height), self.dtype)
         x, y = tf.meshgrid(x, y)  # x and y have shape [height, width]
 
-        return self.gaussian_weight_map(x, y, ref_points)
-
-    def gaussian_weight_map(self, x: tf.Tensor, y: tf.Tensor, ref_points: tf.Tensor) -> tf.Tensor:
-        """ From the x and y meshgrids and the reference points create the gaussian like weight map.
-
-        Args:
-            x: A 2-D tensor of float and shape [height, width] meshgrid
-            y: A 2-D tensor of float and shape [height, width] meshgrid
-            ref_points: A tensor of shape [batch_size, N, heads, (y, x, h, w)] in [0, 1]
-
-        Returns:
-            weight_map: A 4D tensor of float 32 and shape
-                [batch_size, N, heads, height, width] representing
-                a weight map per reference points.
-        """
+        ref_points = tf.transpose(ref_points, (0, 2, 1, 3))
         y_cent, x_cent, height, width = tf.split(ref_points, 4, axis=-1)
 
-        # [batch_size, N, heads, 1] => [batch_size, N, heads, 1, 1]
+        # [batch_size, heads, N,  1] => [batch_size,heads, N,  1, 1]
         y_cent, x_cent = y_cent[..., tf.newaxis], x_cent[..., tf.newaxis]
         height, width = height[..., tf.newaxis], width[..., tf.newaxis]
 
         # [height, width] => [1, 1, 1, height, width]
         y, x = y[tf.newaxis, tf.newaxis, tf.newaxis], x[tf.newaxis, tf.newaxis, tf.newaxis]
 
-        # [batch_size, N, heads, height, width]
+        # [batch_size, heads, N, height, width]
         x_term = -(x - x_cent)**2 / (self._beta * width**2)
         y_term = -(y - y_cent)**2 / (self._beta * height**2)
-        return tf.math.exp(x_term + y_term)
+        weight_map = tf.math.exp(x_term + y_term)
+
+        batch_size, num_heads = tf.shape(weight_map)[0], tf.shape(weight_map)[1]
+        return tf.reshape(weight_map, (batch_size, num_heads, -1, height * width))
 
     def get_config(self):
         config = super().get_config()
